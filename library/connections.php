b0d16704214b4e044ec5352ed18e03fdea3f28ef
MAlejandroFlores <alexfloresm@byui.edu>
2021-01-23 22:40:10 -0800

    Add sql queries and PDO

---
+++ library/connections.php
@@ -0,0 +1,27 @@
+<?php
+/* 
+* Proxy connections to phpmotors database
+*/
+
+
+function phpmotorsConnect()
+{
+    $server = 'localhost';
+    $dbname = 'phpmotors';
+    $username = 'iClient';
+    $password = 'sMyrxcLqiu2M8E4x';
+    $dsn = "mysql:host=$server;dbname=$dbname";
+    $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+
+    // Create the actual connection object and assign it to a variable
+    try {
+        $link = new PDO($dsn, $username, $password, $options);
+        if (is_object($link)) {
+            //echo 'It worked!';
+        }
+    } catch (PDOException $e) {
+        //echo "It didn't work, error: " . $e->getMessage();
+        header('Location: /phpmotors/view/500.php');
+    }
+}
+phpmotorsConnect();
