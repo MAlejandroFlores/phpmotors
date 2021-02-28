810a7be1012860f2d9bd2ee73eacfb5776033ec4
MAlejandroFlores <alexfloresm@byui.edu>
2021-02-27 22:05:41 -0800

    Enhancement 6 complete

--- view/add-vehicle.php
+++ view/add-vehicle.php
@@ -1,4 +1,12 @@
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/header.php'; ?>
+
+<?php
+//If Not seesion login or Client level > 1, redirect to home
+if (!(isset($_SESSION['loggedin']) && ($_SESSION['clientData']['clientLevel'] > '1') ) )  {
+    header( 'Location: ../index.php');
+}
+?>
+
 <?php
 $classificationList = '<select id="classification_id" name="classificationId">';
 foreach ($classifications as $classification) {
