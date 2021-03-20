0cf5d68b1dcbd858de6eeabcf977eb485c9b8275
MAlejandroFlores <alexfloresm@byui.edu>
2021-03-20 13:40:13 -0700

    week 11

---
+++ view/image-admin.php
@@ -0,0 +1,5 @@
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/header.php'; ?>
+
+<h1> Image Management here </h1>
+
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/footer.php'; ?> 
